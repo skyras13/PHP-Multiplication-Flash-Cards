9604ef60aab6a1c5a81be40d1468e6668a2e8c84
Skyler Rasmussen <skyrasmussen13@gmail.com>
2022-06-19 17:32:22 -1000

    adding project to github

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <?php if ($number) { ?>
+        <a href="index.php">Click Here to Pick Another Number!</a>
+    <?php } else { ?>
+        🚀🚀🚀🚀🚀🚀
+    <?php } ?>
+</footer>
---
+++ view/form.php
@@ -0,0 +1,16 @@
+<main class="main-entry">
+    <h2 id="enterNumber">Enter a Number:</h2>
+    <form class="main-entry__form" action="<?php $_SERVER['PHP_SELF'] ?>" method="GET">
+        <input
+            class="main-entry__input"
+            id="num"
+            name="num"
+            aria-labelledby="enterNumber"
+            type="text"
+            maxlength="2"
+            autofocus
+            required
+        >
+        <button class="main-entry__button">Go!</button>
+    </form>
+</main>
